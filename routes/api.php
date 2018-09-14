31894010a25ec38796c94d0f8dffa0cf45421467
PacoMorales <ErJavo_Zarate@hotmail.com>
2018-09-14 13:45:25 -0500

    apiPOST4

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('padrino/nuevo-padrino/app','PRE_ALTA_Controller@nuevoPadrinoAPP')->name('padrino.nuevo-app');
 Route::post('padrino/nuevo-padrino/iniciativa-privada/app','PRE_ALTA_Controller@nuevoPadrinoIPAPP')->name('padrino.nuevo-ip-app');
 
-Route::get('padrino/municipios','PRE_ALTA_Controller@apiMunicipios')->name('padrino.municipios');
+/*Route::get('padrino/municipios','PRE_ALTA_Controller@apiMunicipios')->name('padrino.municipios');
 Route::get('padrino/sectores','PRE_ALTA_Controller@apiSectores')->name('padrino.sectores');
 Route::get('padrino/sectores/estructuras/{id}','PRE_ALTA_Controller@apiEstructura')->name('padrino.estructuras');
-Route::get('padrino/sectores/estructuras/dependencia/{id}','PRE_ALTA_Controller@apiDependencia')->name('padrino.dependencia');
+Route::get('padrino/sectores/estructuras/dependencia/{id}','PRE_ALTA_Controller@apiDependencia')->name('padrino.dependencia');*/
